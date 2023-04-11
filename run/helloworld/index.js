b514808883562fb9ff1f1424b7f8b2dc2114f964
jimyan <jimyan@tencent.com>
2023-04-11 18:43:19 +0800

    update: v9

--- run/helloworld/index.js
+++ run/helloworld/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello v8... ${name}!`);
+  res.send(`Hello v9... ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
